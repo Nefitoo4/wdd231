eebd61c24688f0f96746554264ac2684cc693703
Nefitoo4 <nefizaldana@gmail.com>
2025-01-11 11:07:49 -0600

    fifth_commit

--- week01/scripts/home_page2.js
+++ week01/scripts/home_page2.js
@@ -1,14 +1,22 @@
 /*-----COURSE CERTIFICATION-----*/
 function filterCourses(category) {
   const courses = document.querySelectorAll(".course_card, .course_card_pend");
+  let visibleCourses = 0;
 
   courses.forEach((course) => {
     if (category === "all") {
       course.style.display = "flex";
+      visibleCourses++;
     } else if (course.classList.contains(category)) {
       course.style.display = "flex";
+      visibleCourses++;
     } else {
       course.style.display = "none";
     }
   });
+
+  const totalCredits = visibleCourses * 2;
+  document.getElementById(
+    "total_credits"
+  ).textContent = `The total number of credits for the courses listed below is: ${totalCredits}`;
 }
