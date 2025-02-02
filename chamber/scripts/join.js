0cb9ddf409068ce9c5fb83669ca9d9818035d289
Nefitoo4 <nefizaldana@gmail.com>
2025-02-02 00:59:24 -0600

    commit_02

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,3 +1,4 @@
+//model card in membership card button
 document.querySelectorAll(".model_membership").forEach((button) => {
   button.addEventListener("click", (event) => {
     const dialogId = event.target.nextElementSibling.id;
@@ -11,3 +12,29 @@ document.querySelectorAll(".close").forEach((button) => {
     dialog.close();
   });
 });
+
+//registration form
+document
+  .getElementById("registration_form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const form = event.target;
+    const data = new URLSearchParams(new FormData(form));
+
+    window.location.href = `thankyou.html?${data.toString()}`;
+  });
+
+//thankyou page display data
+const urlParams = new URLSearchParams(window.location.search);
+const formData = {};
+urlParams.forEach((value, key) => {
+  formData[key] = value;
+});
+
+const formDataDiv = document.getElementById("form-data");
+for (const [key, value] of Object.entries(formData)) {
+  const p = document.createElement("p");
+  p.textContent = `${key}: ${value}`;
+  formDataDiv.appendChild(p);
+}
